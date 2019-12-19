e1762e5cafe248ec05cf841334cf5b513b8e4c2d
Michiel van der Geest <hello@michielvandergeest.com>
2019-12-19 12:27:04 -0300

    Fixed path to http-server.

--- src/actions/serve.js
+++ src/actions/serve.js
@@ -1,5 +1,10 @@
 const execa = require('execa')
+const path = require('path')
 
 module.exports = () => {
-  execa('./node_modules/.bin/http-server', ['./dist', '-o', '-c-1']).stdout.pipe(process.stdout)
+  execa(path.join(__dirname, '../..', 'node_modules/.bin/http-server'), [
+    './dist',
+    '-o',
+    '-c-1',
+  ]).stdout.pipe(process.stdout)
 }
