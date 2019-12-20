4433ed06b5a26784b5aafb4ed02e4a384622d323
Michiel van der Geest <hello@michielvandergeest.com>
2019-12-20 11:00:46 -0300

    Fixed imports of plugins in es5 rollup config.

--- src/configs/rollup.es5.config.js
+++ src/configs/rollup.es5.config.js
@@ -1,6 +1,9 @@
 const babel = require('rollup-plugin-babel')
 const resolve = require('rollup-plugin-node-resolve')
 const commonjs = require('rollup-plugin-commonjs')
+const babelPresentEnv = require('@babel/preset-env')
+const babelPluginTransFormSpread = require('@babel/plugin-transform-spread')
+const babelPluginTransFormParameters = require('@babel/plugin-transform-parameters')
 
 module.exports = {
   plugins: [
@@ -9,7 +12,7 @@ module.exports = {
     babel({
       presets: [
         [
-          '@babel/env',
+          babelPresentEnv,
           {
             targets: {
               chrome: '44',
@@ -21,7 +24,7 @@ module.exports = {
           },
         ],
       ],
-      plugins: ['@babel/plugin-transform-spread', '@babel/plugin-transform-parameters'],
+      plugins: [babelPluginTransFormSpread, babelPluginTransFormParameters],
     }),
   ],
   output: {
