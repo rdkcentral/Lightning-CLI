88135c173cb1b61758a7aa3c28f07a3179dd27a0
Michiel van der Geest <hello@michielvandergeest.com>
2020-04-17 16:07:26 -0300

    Changed dist command to accept options (--es5, --es6, --spark) instead of interactive menu.

--- src/actions/dist.js
+++ src/actions/dist.js
@@ -3,42 +3,47 @@ const fs = require('fs')
 const sequence = require('../helpers/sequence')
 const buildHelpers = require('../helpers/build')
 const distHelpers = require('../helpers/dist')
-const ask = require('../helpers/ask')
 
-const askDistType = () =>
-  ask('What type of distributable do you want to create?', null, 'list', ['Es6', 'Es5', 'Spark'])
-
-module.exports = () => {
+module.exports = types => {
   const baseDistDir = path.join(process.cwd(), 'dist')
 
-  let type
-  let distDir
-  return sequence([
-    () => buildHelpers.ensureCorrectGitIgnore(),
-    () => askDistType().then(val => (type = val.toLowerCase())),
-    () => {
-      distDir = path.join(baseDistDir, type)
-    },
-    () => {
-      if (!fs.existsSync(distDir)) {
-        return sequence([
-          () => buildHelpers.ensureFolderExists(distDir),
-          () => buildHelpers.ensureFolderExists(path.join(distDir, 'js')),
-          () => distHelpers.setupDistFolder(distDir, type),
-        ])
-      }
-      return true
-    },
-    () => buildHelpers.removeFolder(path.join(distDir, 'static')),
-    () => buildHelpers.copyStaticFolder(distDir),
-    () =>
-      type === 'es6' &&
-      buildHelpers.bundleEs6App(path.join(distDir, 'js'), {}, { sourcemaps: false }),
-    () =>
-      type === 'es5' &&
-      buildHelpers.bundleEs5App(path.join(distDir, 'js'), {}, { sourcemaps: false }),
-    () =>
-      type === 'spark' &&
-      buildHelpers.bundleSparkApp(path.join(distDir, 'js'), {}, { sourcemaps: false }),
-  ])
+  const dist = type => {
+    let distDir
+    return sequence([
+      () => buildHelpers.ensureCorrectGitIgnore(),
+      () => {
+        distDir = path.join(baseDistDir, type)
+      },
+      () => {
+        if (!fs.existsSync(distDir)) {
+          return sequence([
+            () => buildHelpers.ensureFolderExists(distDir),
+            () => buildHelpers.ensureFolderExists(path.join(distDir, 'js')),
+            () => distHelpers.setupDistFolder(distDir, type),
+          ])
+        }
+        return true
+      },
+      () => buildHelpers.removeFolder(path.join(distDir, 'static')),
+      () => buildHelpers.copyStaticFolder(distDir),
+      () =>
+        type === 'es6' &&
+        buildHelpers.bundleEs6App(path.join(distDir, 'js'), {}, { sourcemaps: false }),
+      () =>
+        type === 'es5' &&
+        buildHelpers.bundleEs5App(path.join(distDir, 'js'), {}, { sourcemaps: false }),
+      () =>
+        type === 'spark' &&
+        buildHelpers.bundleSparkApp(path.join(distDir, 'js'), {}, { sourcemaps: false }),
+    ])
+  }
+
+  // execute the dist function for all types
+  return types.reduce((promise, type) => {
+    return promise
+      .then(function() {
+        return dist(type)
+      })
+      .catch(Promise.reject)
+  }, Promise.resolve(null))
 }
