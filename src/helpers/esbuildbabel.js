18de925c1ec44862a304be8b3c51cad4207737a9
sandeep-vedam <sandeepkumar.vedam@soctronics.com>
2021-04-21 21:55:28 +0530

    Updated licence

--- src/helpers/esbuildbabel.js
+++ src/helpers/esbuildbabel.js
@@ -2,19 +2,12 @@
  * If not stated otherwise in this file or this component's LICENSE file the
  * following copyright and licenses apply:
  *
- * Copyright 2020 Metrological
+ * ISC License
+ * Copyright (c) Antoine Boulanger (https://github.com/antoineboulanger)
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * Licensed under the Apache License, Version 2.0 (the License);
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 const babel = require('@babel/core')
