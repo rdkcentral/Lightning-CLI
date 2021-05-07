598e1d4704cd2cee6146d31e4e87ef4c3505ba27
Sarang Kartikey <sarang.kartikey@hotstar.com>
2021-05-07 18:36:28 +0530

    added process.env.NODE_ENV support esbuild

--- src/configs/esbuild.es5.config.js
+++ src/configs/esbuild.es5.config.js
@@ -2,7 +2,7 @@
  * If not stated otherwise in this file or this component's LICENSE file the
  * following copyright and licenses apply:
  *
- * Copyright 2020 RDK Management
+ * Copyright 2020 Metrological
  *
  * Licensed under the Apache License, Version 2.0 (the License);
  * you may not use this file except in compliance with the License.
@@ -37,6 +37,7 @@ module.exports = (folder, globalName) => {
     acc[`process.env.${key}`] = `"${appVars[key]}"`
     return acc
   }, {})
+  defined['process.env.NODE_ENV'] = `"${process.env.NODE_ENV}"`
 
   return {
     plugins: [
