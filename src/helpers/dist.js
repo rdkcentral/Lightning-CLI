bdd9bc2bd1967ef0eb3ebcabe9dd78e50b941227
Jan Sunavec <jan.sunavec@gmail.com>
2021-08-18 07:24:59 +0200

    lightning.js -> lightning.min.js

--- src/helpers/dist.js
+++ src/helpers/dist.js
@@ -33,7 +33,7 @@ const setupDistFolder = (folder, type, metadata) => {
   if (type === 'es6') {
     shell.cp(
       path.join(nodeModulesPath, 'dist/lightning.min.js'),
-      path.join(folder, 'js', 'lightning.js')
+      path.join(folder, 'js', 'lightning.min.js')
     )
 
     shell.cp(
@@ -45,7 +45,7 @@ const setupDistFolder = (folder, type, metadata) => {
     const lightningFile = path.join(nodeModulesPath, 'dist/lightning.es5.min.js')
     // lightning es5 bundle in dist didn't exist in earlier versions (< 1.3.1)
     if (fs.existsSync(lightningFile)) {
-      shell.cp(lightningFile, path.join(folder, 'js', 'lightning.es5.js'))
+      shell.cp(lightningFile, path.join(folder, 'js', 'lightning.es5.min.js'))
     }
 
     shell.cp(
