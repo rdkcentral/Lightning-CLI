8729a06a389515f5f0bdcb88398f4e2516af2a1b
Maikel Brons <m.brons@metrological.com>
2023-03-15 16:54:03 +0100

    lng create tests

--- tests/globalSetup.js
+++ tests/globalSetup.js
@@ -1,32 +1,29 @@
-const fs = require('fs-extra');
-const { addAttach } = require("jest-html-reporters/helper");
+const fs = require('fs-extra')
 
-module.exports = async function (globalConfig, projectConfig) {
+module.exports = async function(globalConfig, projectConfig) {
   global.setEnvironmentValue = (key, value) => {
     //Check if the .env exists if not create it
-    if (!fs.existsSync('.env'))
-      fs.writeFileSync( '.env', '')
+    if (!fs.existsSync('.env')) fs.writeFileSync('.env', '')
 
     // read the contents of the .env file into a string
-    let envFile = fs.readFileSync('.env', 'utf-8');
+    let envFile = fs.readFileSync('.env', 'utf-8')
 
     // split the string into an array of lines
-    let envLines = envFile.split('\n');
+    let envLines = envFile.split('\n')
 
     // find the line that contains the variable you want to update
-    let myVarLine = envLines.findIndex(line => line.startsWith(`${key}=`));
+    let myVarLine = envLines.findIndex(line => line.startsWith(`${key}=`))
 
     //when the variable doesn't exists create it.
-    if (myVarLine == -1)
-      envLines[envLines.length] = `${key}=${value}`;
-    else // update the value of the variable
-      envLines[myVarLine] = `${key}=${value}`;
+    if (myVarLine == -1) envLines[envLines.length] = `${key}=${value}`
+    // update the value of the variable
+    else envLines[myVarLine] = `${key}=${value}`
 
     // join the lines back into a string and write it to the .env file
-    fs.writeFileSync('.env', envLines.join('\n'));
+    fs.writeFileSync('.env', envLines.join('\n'))
   }
 
-  global.changeSettingsJson = (obj) => {
+  global.changeSettingsJson = obj => {
     const file = 'settings.json'
 
     let settingsJson = JSON.parse(fs.readFileSync(file))
@@ -34,28 +31,28 @@ module.exports = async function (globalConfig, projectConfig) {
     if (obj.platformSettings)
       settingsJson.platformSettings = {
         ...settingsJson.platformSettings,
-        ...obj.platformSettings
+        ...obj.platformSettings,
       }
 
     fs.writeFileSync(file, JSON.stringify(settingsJson, null, 2))
     return settingsJson
   }
 
-  global.changeEsEnv = (esEnv) => {
+  global.changeEsEnv = esEnv => {
     const obj = {
       platformSettings: {
-        esEnv: esEnv
-      }
+        esEnv: esEnv,
+      },
     }
-    return changeSettingsJson(obj)
+    return global.changeSettingsJson(obj)
   }
-  global.changeShowVersion = (show) => {
+  global.changeShowVersion = show => {
     const obj = {
       platformSettings: {
-        showVersion: show
-      }
+        showVersion: show,
+      },
     }
-    return changeSettingsJson(obj)
+    return global.changeSettingsJson(obj)
   }
 
   global.DelayTest = new Promise(resolve => {
@@ -65,9 +62,9 @@ module.exports = async function (globalConfig, projectConfig) {
   const appName = 'My Awesome App'
   global.appConfig = {
     name: appName,
-    id: `com.metrological.app.${appName.replace(/[^A-Z0-9]/gi, '')}`,
-    appPath: process.cwd() + `/com.metrological.app.${appName.replace(/[^A-Z0-9]/gi, '')}`
+    id: `com.domain.app.${appName.replace(/[^A-Z0-9]/gi, '')}`,
+    appPath: process.cwd() + `/com.domain.app.${appName.replace(/[^A-Z0-9]/gi, '')}`,
   }
 
-  global.originalCWD = process.cwd();
-};
+  global.originalCWD = process.cwd()
+}
--- tests/globalTeardown.js
+++ tests/globalTeardown.js
@@ -1,5 +1,8 @@
 const opener = require('opener')
-module.exports = async function (globalConfig, projectConfig) {
+const fs = require('fs-extra')
+module.exports = async function(globalConfig, projectConfig) {
+  // Clean up the test by deleting the app folder
+  fs.removeSync(`${global.appConfig.appPath}`)
   console.log('Finish up tests')
   opener('./tests_report/report.html')
-};
+}
